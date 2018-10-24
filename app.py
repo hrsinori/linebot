6968bed974f9bd2186f7ee0dc79ee2caa6288a31
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-10-24 15:06:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,48 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Nice to meet you!')
+    message = TextSendMessage(text='您好，請問今天有什麼事要訴說呢？')
     line_bot_api.reply_message(event.reply_token, message)
 
+string MyLineChannelAccessToken = "隱藏起來";
+/// <summary>
+/// </summary>
+/// <returns></returns>
+[HttpPost]
+[Route("api/LineBotApi/post")]
+public async void Post()
+{
+    try
+    {
+        string postData = Request.Content.ReadAsStringAsync().Result;
+        var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+        var bot = new isRock.LineBot.Bot(this.MyLineChannelAccessToken);
+        string userCommandString = ReceivedMessage.events[0].message.text;
+        LineBotMember _service = new LineBotMember();
+        string message = string.Empty;
+
+        if (userCommandString == "save")
+        {
+            _service.InsertID(ReceivedMessage.events[0].source.userId);
+            var call = Task.Run(() =>
+            {
+                bot.ReplyMessage(ReceivedMessage.events[0].replyToken,
+                    string.Format("紀錄成功: {0}", ReceivedMessage.events[0].source.userId));
+            });
+        }
+     }
+      catch (Exception ex)
+      {
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                var bot = new isRock.LineBot.Bot(this.MyLineChannelAccessToken);
+                var call = Task.Run(() =>
+                {
+                    bot.ReplyMessage(ReceivedMessage.events[0].replyToken,
+                        string.Format("錯誤資訊:{0}", ex.Message));
+                });
+       }
+}
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
