632d8f28b39b62e5a3bdce69c39fccbe8ba8e5e8
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-10-24 19:08:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
     
     message = TextSendMessage(text='告解少女')
     message2 = TextSendMessage(event.source.user_id)
-    line_bot_api.reply_message(event.reply_token, [message message2])
+    line_bot_api.reply_message(event.reply_token, [message ,message2])
     
     
 
