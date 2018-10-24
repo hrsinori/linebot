c926d556091ee12018280662cb10f50554be79e4
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-10-24 18:43:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,9 @@ def handle_message(event):
     
     message = TextSendMessage(text='告誡少女')
     line_bot_api.reply_message(event.reply_token, message)
-
+    
+    message = TextSendMessage(event.source.user_id)
+    line_bot_api.reply_message(event.reply_token, message)
     
 
 import os
