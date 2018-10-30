69b9c8052287a284d96a6ad607d6f6e2921b02a2
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-10-30 11:43:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,12 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
+# 處理訊息
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token, message)
     elif '傷心' in _token[0] or '難過' in _token[0] or '厭世' in _token[0] or '憂鬱' in _token[0]:
         _message = TextSendMessage(text = '請問發生什麼事了嗎？')	#reply course
         line_bot_api.reply_message(event.reply_token, _message) 
