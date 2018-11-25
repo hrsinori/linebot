b4563f9e6721cfd226ec1c9c25fcd00ca61ea126
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-11-26 00:08:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,23 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-        
+  
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+#     _message = TextSendMessage(text='Nice to meet you!')
+#     _message = TextSendMessage(text=(event.source.user_id)) #reply userid
+#     line_bot_api.reply_message(event.reply_token, _message)  
+    # message = TextSendMessage(text=event)
+#     print(event)
+text = event.message.text
+from snownlp import SnowNLP
+s = SnowNLP(text)
+for sentence in s.sentences:
+     print(sentence)
+s1 = SnowNLP(s.sentences[0])
+s1.sentiments
+line_bot_api.reply_message(event.reply_token, s1)
             
 import os
 if __name__ == "__main__":
@@ -54,3 +70,5 @@ if __name__ == "__main__":
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+    
