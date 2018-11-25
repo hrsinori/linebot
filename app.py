b334f9b4ff4318750ebc99caf46774379bfa8526
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-11-26 00:39:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,8 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, s1)
             
 import os
+
 if __name__ == "__main__":
-    # load PM2.5 records
-    loadPMJson()
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
