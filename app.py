56c5ec289d96847ac2febdac31616d7800106093
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-11-26 15:59:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def handle_message(event):
     # text = u +text
     s = SnowNLP(text)
     s1 = SnowNLP(s.sentences[0])
-    s1 = TextSendMessage(text = s1.sentiments)
+    # s1 = TextSendMessage(text = s1.sentiments)
     if 0.1 > s1 and s1 >= 0:
         message1 = TextSendMessage(text='請問您還好嗎？')
         line_bot_api.reply_message(event.reply_token, message1)
