d28f7e47e8fb0f0d0784a9b5127c56b8990ae972
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-11-27 12:05:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def handle_message(event):
 #     print(event)
 
     text= event.message.text
-    if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' :
+    if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' or text == '開心' or text == '悲傷' :
        pass
     # text = u +text
     else :
