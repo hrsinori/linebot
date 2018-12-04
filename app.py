1b5b014774c62d88698866ce2c288ef07387f92d
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-12-04 09:59:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_message(event):
         pass
         #GDriveJSON就輸入下載下來Json檔名稱
         #GSpreadSheet是google試算表名稱
-        GDriveJSON = 'file.txt'
+        GDriveJSON = 'time.json'
         GSpreadSheet = 'time'
         while True:
             try:
