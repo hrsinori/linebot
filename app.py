efd149b0e211f66422764fdf7ce5d288a78cc648
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-12-04 10:33:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def handle_message(event):
         GSpreadSheet = 'time'
         while True:
             try:
-                scope = ['https://docs.google.com/spreadsheets/d/1c9xHwgBWUH4XW6EhGDdi6uIwH9IkQ8nq3fcAA2PL-Go/edit#gid=0']
+                scope = ['https://spreadsheets.google.com/feeds']
                 key = SAC.from_json_keyfile_name(GDriveJSON, scope)
                 gc = gspread.authorize(key)
                 worksheet = gc.open(GSpreadSheet).sheet1
