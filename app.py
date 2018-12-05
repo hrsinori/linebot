06fe0f50e8f958da9f6d185fd4a18a0cf246117b
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-12-06 04:39:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,14 +62,7 @@ def handle_message(event):
 #     line_bot_api.reply_message(event.reply_token, _message)  
     # message = TextSendMessage(text=event)
 #     print(event)    
-'''
-    text= event.message.text
-    s = SnowNLP(text)
-    s1 = SnowNLP(s.sentences[0])
-    s1.sentiments 
-    s1 = TextSendMessage(text = s1.sentiments)    
-    line_bot_api.reply_message(event.reply_token, s1)
-'''    
+    
     if event.message.text != "":
         message = TextSendMessage(text='紀錄成功')
         line_bot_api.reply_message(event.reply_token,message)
@@ -95,6 +88,12 @@ def handle_message(event):
                 return textt   
 
 '''
+    text= event.message.text
+    s = SnowNLP(text)
+    s1 = SnowNLP(s.sentences[0])
+    s1.sentiments 
+    s1 = TextSendMessage(text = s1.sentiments)    
+    line_bot_api.reply_message(event.reply_token, s1)
     if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' or text == '開心' or text == '悲傷' or text == '對阿' or text == '對啊' :
        pass
     elif text == '好的' or text == '知道了' or text == '好喔' :
