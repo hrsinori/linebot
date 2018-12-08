618f55009532a570917d222c75ae7e709e224634
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2018-12-08 15:21:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,15 +79,15 @@ def handle_message(event):
                     key = SAC.from_json_keyfile_name(GDriveJSON, scope)
                     gc = gspread.authorize(key)
                     worksheet = gc.open(GSpreadSheet).sheet1
-            except Exception as ex:
-                print('無法連線Google試算表', ex)
-                sys.exit(1)
-            textt=""
-            textt+=event.message.text
-            if textt!="":
-                worksheet.append_row((str(datetime.datetime.now()), count))
-                print('新增一列資料到試算表' ,GSpreadSheet)
-                return textt 
+                except Exception as ex:
+                    print('無法連線Google試算表', ex)
+                    sys.exit(1)
+                textt=""
+                textt+=event.message.text
+                if textt!="":
+                    worksheet.append_row((str(datetime.datetime.now()), count))
+                    print('新增一列資料到試算表' ,GSpreadSheet)
+                    return textt 
 '''
     s = SnowNLP(text)
     s1 = SnowNLP(s.sentences[0])
