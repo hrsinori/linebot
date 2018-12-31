d7ad6daab7c715d4feb16dcd6225e1a13c9636fa
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2019-01-01 05:15:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handle_message(event):
     text = event.message.text
     s = SnowNLP(text)
     s1 = SnowNLP(s.sentences[0])
-    s1.sentiments = text
+    s1.sentiments
     
     if text != "":
         #GDriveJSON就輸入下載下來Json檔名稱
@@ -84,9 +84,9 @@ def handle_message(event):
                     print('無法連線Google試算表', ex)
                     sys.exit(1)
                 textt=""
-                textt+= text
+                textt+= s1.sentiments
                 if textt!="":
-                    worksheet.append_row((str(datetime.datetime.now()),textt))
+                    worksheet.append_row((float(datetime.datetime.now()),textt))
                     print('新增一列資料到試算表' ,GSpreadSheet)
                     return textt 
     '''            
