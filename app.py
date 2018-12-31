e0385033cdafc8ea9153d1083d907d78e8acf8b3
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2019-01-01 05:23:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def handle_message(event):
     s1 = TextSendMessage(text = s1.sentiments)    
     line_bot_api.reply_message(event.reply_token, s1)
     '''
-                   
+    text = event.message.text               
     if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' or text == '開心' or text == '悲傷' or text == '對阿' or text == '對啊' :
            pass
 
