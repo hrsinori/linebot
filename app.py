c46543b55e0ac29701f948256caa6db85d4e21ea
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2019-01-01 21:40:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,8 +104,8 @@ def handle_message(event):
     if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' or text == '開心' or text == '悲傷' or text == '對阿' or text == '對啊' :
            pass  
     elif text == '紀錄查詢' :
-            messages_ = TextSendMessage(text = getAverage())
-	    line_bot_api.reply_message(event.reply_token, messages_) 
+            messages = TextSendMessage(text = getAverage())
+	    line_bot_api.reply_message(event.reply_token, messages) 
     elif text == '健康資訊' :
 	    messagex = TextSendMessage(text='希望能幫助到您')
 	    line_bot_api.reply_message(event.reply_token, messagex) 	 
