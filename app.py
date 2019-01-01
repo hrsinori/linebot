f2dbfb37ecd98a564f77055824492893eeb9be5f
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2019-01-01 08:54:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def handle_message(event):
                 worksheet.append_row((str(datetime.datetime.now()),textt))
                 print('新增一列資料到試算表' ,GSpreadSheet)
                 return textt
-	    if (event.message.text == "紀錄查詢")
+        if (event.message.text == "紀錄查詢")
 			{
 			    var result = _service.Getnumber(ReceivedMessage.events[0].source.number);
 
