c8f033c3b0970899b64e2d146b07b98f499901f9
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2019-01-01 21:39:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,6 @@ def getAverage():
         average = total / count
     
     return average #average -1表示計算異常 
-    a = average
-    b = str(a)
 
 app = Flask(__name__)
 
@@ -106,9 +104,8 @@ def handle_message(event):
     if text == '謝謝' or text == '謝謝你' or text == '幹' or text == '去你的' or text == '開心' or text == '悲傷' or text == '對阿' or text == '對啊' :
            pass  
     elif text == '紀錄查詢' :
-	    messages = TextSendMessage(text='近期情緒指數')
-            messages_ = TextSendMessage(text = a)
-	    line_bot_api.reply_message(event.reply_token, messages,messages_) 
+            messages_ = TextSendMessage(text = getAverage())
+	    line_bot_api.reply_message(event.reply_token, messages_) 
     elif text == '健康資訊' :
 	    messagex = TextSendMessage(text='希望能幫助到您')
 	    line_bot_api.reply_message(event.reply_token, messagex) 	 
