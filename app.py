342bd3f94764cb6c2a90b6f6ea030cd243b811f0
hrsinori0214 <43977901+hrsinori0214@users.noreply.github.com>
2021-02-07 04:23:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,36 @@
-from flask import Flask, request, abort
-
-import urllib.request, json
-import requests
-from bs4 import BeautifulSoup
-
 import os
-import sys
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
 
-from snownlp import SnowNLP
-from snownlp import seg
-
-import sys
-import datetime
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials as SAC
-from selenium import webdriver
-
+from flask import Flask
+from linebot import LineBotApi, WebhookHandler
 
 app = Flask(__name__)
 
-ACCESS_TOKEN= os.environ['ACCESS_TOKEN']
-SECRET= os.environ['CHANNEL_SECRET']
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
-# Channel Access Token
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-# Channel Secret
-handler = WebhookHandler(SECRET)
+# 利用 handler 處理 LINE 觸發事件
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-pm_site = {}
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
+    )
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
-# 監聽所有來自 /callback 的 Post Request
+# 利用 route 處理路由
+from flask import request, abort
+from linebot.exceptions import InvalidSignatureError
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-#     _message = TextSendMessage(text='Nice to meet you!')
-#     _message = TextSendMessage(text=(event.source.user_id)) #reply userid
-#     line_bot_api.reply_message(event.reply_token, _message)  
-    # message = TextSendMessage(text=event)
-#     print(event)    
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
-  
-import os
-
-if __name__ == "__main__":
-    
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-    
